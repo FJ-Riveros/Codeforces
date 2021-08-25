713902e1829962987faa0b5ce345156d258880b4
FJ-Riveros <fran.etnies@gmail.com>
2021-08-25 16:21:35 +0200

    1537-B Bad Boy

--- Bad_Boy.java
+++ Bad_Boy.java
@@ -3,44 +3,20 @@ import java.util.Arrays;
 import java.util.Collections;  
 import java.util.List;
 import java.util.ArrayList;
- //https://codeforces.com/problemset/problem/1537/B
 
 public class Bad_Boy{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		int tests = scanner.nextInt();
 		int[][] arr;
+		int farY = 0, farX = 0;
 		
 		for(int i=0; i<tests; i++){
 			int n = scanner.nextInt();
 			int m = scanner.nextInt();
-			int posX = scanner.nextInt() - 1;
-			int posY = scanner.nextInt() - 1;
-			int firstBallX = 0;
-			int firstBallY = 0;
-			int secondBallX = 0;
-			int secondBallY = 0;
-			int maxX = 0;
-			int maxY = 0;
-			for(int x=0; x<n; x++){
-				for(int y=0; y<m; y++){
-					if(maxY < Math.abs(posY - y)) maxY = Math.abs(posY - y);
-				}
-				if(maxX < Math.abs(posX - x)) maxX = Math.abs(posX - x);
-			}
-			
-			for(int x=0; x<n; x++){
-				for(int y=0; y<m; y++){
-					if(maxY == y && maxX == x){
-						System.out.print("X");
-					}else if(posX == x && posY == y){ 
-						System.out.print("P");
-					}else{
-						System.out.print("O");
-					}
-				}
-				System.out.println();
-			}
+			int posY = scanner.nextInt();
+			int posX = scanner.nextInt();
+            System.out.println("1 1 " + n + " " + m); 
 		}
 	}
 	
